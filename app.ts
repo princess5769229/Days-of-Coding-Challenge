91762471ab542fa47dffd229e391ac7757a25d25
princess5769229 <168623931+princess5769229@users.noreply.github.com>
2024-05-04 07:02:12 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,48 @@
+// Day 10 CODING CHALLENGE
+
+// Question 1: No Users: Ensure your user list is not empty.
+
+let usernames: string[]= [];
+
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}else{
+    // Great users
+}
+
+// Question 2: Checking Usernames: Do the following to create a paragraph that simulates how websites ensure 
+// that everyone has a unique usernames.
+
+let current_users: string[]= ["saba", "admin", "ahmed", "babar", "raza"];
+let new_users: string[]= ["fawad", "hussain", "user7", "admin", "User9"];
+
+new_users.forEach((newUser) => {
+    if (
+        current_users.some(
+            (currentUser) => currentUser.toLowerCase() === newUser. toLowerCase() 
+        )
+    ) {
+        console.log(`${newUser} will need to enter a new username.` );
+    }else{
+        console.log(`${newUser} is available`);
+    }
+});
+
+// Question 3: Ordinal Numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in the, 
+// except 1, 2, and 3.
+
+let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for loop
+for(let i = 0; i < myNumbers.length; i++){
+    // now we use conditions 
+    if(myNumbers[i] == 1){
+        console.log(`${myNumbers[i]}st`);
+    }else if(myNumbers[i] == 2){
+        console.log(`${myNumbers[i]}nd`);
+
+    }else if(myNumbers[i] === 3){
+        console.log(`${myNumbers[i]}rd`);
+
+    }else if(myNumbers[i] >= 4 && myNumbers[i] <= 9)
+        console.log(`${myNumbers[i]}th`);
+    }
